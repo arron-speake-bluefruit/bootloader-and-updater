c48d8e9e8e9222bfcc4edbd5c1bc9edab00724d1
Arron Speake <arron.speake@bluefruit.co.uk>
2024-04-15 12:14:04 +0100

    Add empty event queue file

---
+++ application/include/event_queue.h
@@ -0,0 +1,5 @@
+#ifndef EVENT_QUEUE_H
+#define EVENT_QUEUE_H
+
+
+#endif // EVENT_QUEUE_H
---
+++ tests/tests/application_event_queue_tests.c
@@ -0,0 +1,10 @@
+#include "test_assert.h"
+#include "event_queue.h"
+
+static void can_initialize_event_queue(void) {
+
+}
+
+int main(void) {
+    can_initialize_event_queue();
+}
