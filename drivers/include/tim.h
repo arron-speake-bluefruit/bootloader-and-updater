eeaa710cb7a5f905ec060caa310a1af7a8627737
Arron Speake <arron.speake@bluefruit.co.uk>
2024-05-02 10:28:08 +0100

    Add more functions to TIM driver

--- drivers/include/tim.h
+++ drivers/include/tim.h
@@ -4,6 +4,7 @@
 // TIM7 is not exposed, since it is not available on STM32F05 devices.
 
 #include <stdint.h>
+#include <stdbool.h>
 
 typedef enum tim {
     // tim17 = ((uintptr_t)0x40014800),
@@ -16,7 +17,17 @@ typedef enum tim {
     // tim2 = ((uintptr_t)0x40000000),
 } tim_t;
 
-// Enable the given timer peripheral's counter and auto-reload preload buffer.
+// Enable TIMx_ARR register buffering.
+void tim_enable_autoreload_preload(tim_t tim);
+
+// When one-pulse mode is enabled, the counter stops counting when an event is generated,
+// instead of repeating.
+void tim_enable_one_pulse_mode(tim_t tim);
+
+// Returns whether the timer's CR1_CEN bit is set.
+bool tim_is_enabled(tim_t tim);
+
+// Enable the given timer peripheral's counter.
 void tim_enable(tim_t tim);
 
 // Enable the update event interrupt, triggered when the given timer overflows.
@@ -34,5 +45,9 @@ void tim_set_autoreload_value(tim_t tim, uint16_t value);
 // Re-initialize the timer counter and generate an update of its registers. The prescaler counter
 // is cleared (but the prescaler ratio is not affected)
 void tim_generate_event(tim_t tim);
+
+// Get the current counter of the given timer. Basic timers are always upcounting, so this value
+// increases towards the reload value.
+uint16_t tim_get_counter(tim_t tim);
 
 #endif // DRIVERS_TIM_H
--- drivers/source/tim.c
+++ drivers/source/tim.c
@@ -13,6 +13,7 @@
 
 // TIMx CR1 bit offsets
 #define TIMx_CR1_CEN_BIT 0
+#define TIMx_CR1_OPM_BIT 3
 #define TIMx_CR1_ARPE_BIT 7
 
 // TIMx DIER bit offsets
@@ -26,10 +27,29 @@ static inline volatile uint16_t* get_register(tim_t base, uint32_t offset) {
     return (volatile uint16_t*)((uintptr_t)base + offset);
 }
 
+void tim_enable_autoreload_preload(tim_t tim) {
+    volatile uint16_t* cr1 = get_register(tim, TIMx_CR1_OFFSET);
+    CRITICAL_SECTION_ENTER();
+    *cr1 |= (1 << TIMx_CR1_ARPE_BIT);
+    CRITICAL_SECTION_EXIT();
+}
+
+void tim_enable_one_pulse_mode(tim_t tim) {
+    volatile uint16_t* cr1 = get_register(tim, TIMx_CR1_OFFSET);
+    CRITICAL_SECTION_ENTER();
+    *cr1 |= (1 << TIMx_CR1_OPM_BIT);
+    CRITICAL_SECTION_EXIT();
+}
+
+bool tim_is_enabled(tim_t tim) {
+    volatile uint16_t* cr1 = get_register(tim, TIMx_CR1_OFFSET);
+    return (*cr1 & (1 << TIMx_CR1_CEN_BIT)) != 0;
+}
+
 void tim_enable(tim_t tim) {
     volatile uint16_t* cr1 = get_register(tim, TIMx_CR1_OFFSET);
     CRITICAL_SECTION_ENTER();
-    *cr1 |= (1 << TIMx_CR1_CEN_BIT) | (1 << TIMx_CR1_ARPE_BIT);
+    *cr1 |= (1 << TIMx_CR1_CEN_BIT);
     CRITICAL_SECTION_EXIT();
 }
 
@@ -59,4 +79,9 @@ void tim_set_autoreload_value(tim_t tim, uint16_t value) {
 void tim_generate_event(tim_t tim) {
     volatile uint16_t* egr = get_register(tim, TIMx_EGR_OFFSET);
     *egr = 1;
+}
+
+uint16_t tim_get_counter(tim_t tim) {
+    volatile uint16_t* cnt = get_register(tim, TIMx_CNT_OFFSET);
+    return *cnt;
 }
