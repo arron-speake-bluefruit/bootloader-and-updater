21b3b45e42f8204f6eceda527a559d49981c19fa
Arron Speake <arron.speake@bluefruit.co.uk>
2024-05-02 15:22:58 +0100

    Properly reserve RAM for application vector table

--- application/source/vector_table.c
+++ application/source/vector_table.c
@@ -1,8 +1,11 @@
 #include "vector_table.h"
 #include <stdint.h>
 
-extern uintptr_t _app_vectors[];
+enum { vector_count = 48 };
+
+__attribute__ ((section(".app_vectors")))
+uintptr_t app_vectors[vector_count];
 
 void vector_table_set(vector_table_index_t index, vector_table_function_t function) {
-    _app_vectors[index] = (uintptr_t)function;
+    app_vectors[index] = (uintptr_t)function;
 }
