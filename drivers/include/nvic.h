7f67ec8807b737876d92873294b7a6f878f8dbc3
Arron Speake <arron.speake@bluefruit.co.uk>
2024-05-02 10:27:35 +0100

    Add NVIC diable tim6 function

--- drivers/include/nvic.h
+++ drivers/include/nvic.h
@@ -7,4 +7,7 @@ void nvic_enable_usart2_global_interrupt(void);
 // Enable the TIM6 global interrupt.
 void nvic_enable_tim6_global_interrupt(void);
 
+// Disable the TIM6 global interrupt.
+void nvic_disable_tim6_global_interrupt(void);
+
 #endif // DRIVERS_NVIC_H
--- drivers/source/nvic.c
+++ drivers/source/nvic.c
@@ -3,6 +3,7 @@
 #include <stdint.h>
 
 #define NVIC_ISER ((volatile uint32_t*)0xE000E100)
+#define NVIC_ICER ((volatile uint32_t*)0XE000E180)
 
 #define NVIC_ISER_USART2_BIT 28
 #define NVIC_ISER_TIM6_BIT 17
@@ -13,4 +14,8 @@ void nvic_enable_usart2_global_interrupt(void) {
 
 void nvic_enable_tim6_global_interrupt(void) {
     *NVIC_ISER = 1 << NVIC_ISER_TIM6_BIT;
+}
+
+void nvic_disable_tim6_global_interrupt(void) {
+    *NVIC_ICER = 1 << NVIC_ISER_TIM6_BIT;
 }
