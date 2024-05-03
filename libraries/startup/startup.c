ca53b31beffb4dca6fd37015589bcb6a3d40a942
Arron Speake <arron.speake@bluefruit.co.uk>
2024-05-03 15:16:06 +0100

    Cleanup comments in flash vector table

--- libraries/startup/startup.c
+++ libraries/startup/startup.c
@@ -60,45 +60,49 @@ const uintptr_t vectors[] = {
     (uintptr_t)menmanage_handler, // 0x0000'0010 - Memory management
     (uintptr_t)busfault_handler, // 0x0000'0014 - Pre-fetch fault, memory access fault
     (uintptr_t)usagefault_handler, // 0x0000'0018 - Undefined instruction or illegal state
-    (uintptr_t)hardfault_handler, // 0x0000'001C - TODO
-    (uintptr_t)hardfault_handler, // 0x0000'0020 - TODO
-    (uintptr_t)hardfault_handler, // 0x0000'0024 - TODO
-    (uintptr_t)hardfault_handler, // 0x0000'0028 - TODO
-    (uintptr_t)hardfault_handler, // 0x0000'002C - TODO
-    (uintptr_t)hardfault_handler, // 0x0000'0030 - TODO
-    (uintptr_t)hardfault_handler, // 0x0000'0034 - TODO
-    (uintptr_t)hardfault_handler, // 0x0000'0038 - TODO
-    (uintptr_t)hardfault_handler, // 0x0000'003C - TODO
-    (uintptr_t)hardfault_handler, // 0x0000'0040 - TODO
-    (uintptr_t)hardfault_handler, // 0x0000'0044 - TODO
-    (uintptr_t)hardfault_handler, // 0x0000'0048 - TODO
-    (uintptr_t)hardfault_handler, // 0x0000'004C - TODO
-    (uintptr_t)hardfault_handler, // 0x0000'0050 - TODO
-    (uintptr_t)hardfault_handler, // 0x0000'0054 - TODO
-    (uintptr_t)hardfault_handler, // 0x0000'0058 - TODO
-    (uintptr_t)hardfault_handler, // 0x0000'005C - TODO
-    (uintptr_t)hardfault_handler, // 0x0000'0060 - TODO
-    (uintptr_t)hardfault_handler, // 0x0000'0064 - TODO
-    (uintptr_t)hardfault_handler, // 0x0000'0068 - TODO
-    (uintptr_t)hardfault_handler, // 0x0000'006C - TODO
-    (uintptr_t)hardfault_handler, // 0x0000'0070 - TODO
-    (uintptr_t)hardfault_handler, // 0x0000'0074 - TODO
-    (uintptr_t)hardfault_handler, // 0x0000'0078 - TODO
-    (uintptr_t)hardfault_handler, // 0x0000'007C - TODO
-    (uintptr_t)hardfault_handler, // 0x0000'0080 - TODO
-    (uintptr_t)hardfault_handler, // 0x0000'0084 - TODO
-    (uintptr_t)hardfault_handler, // 0x0000'0088 - TODO
-    (uintptr_t)hardfault_handler, // 0x0000'008C - TODO
-    (uintptr_t)hardfault_handler, // 0x0000'0090 - TODO
-    (uintptr_t)hardfault_handler, // 0x0000'0094 - TODO
-    (uintptr_t)hardfault_handler, // 0x0000'0098 - TODO
-    (uintptr_t)hardfault_handler, // 0x0000'009C - TODO
-    (uintptr_t)hardfault_handler, // 0x0000'00A0 - TODO
-    (uintptr_t)hardfault_handler, // 0x0000'00A4 - TODO
-    (uintptr_t)hardfault_handler, // 0x0000'00A8 - TODO
-    (uintptr_t)hardfault_handler, // 0x0000'00AC - TODO
-    (uintptr_t)hardfault_handler, // 0x0000'00B0 - TODO
-    (uintptr_t)hardfault_handler, // 0x0000'00B4 - TODO
-    (uintptr_t)hardfault_handler, // 0x0000'00B8 - TODO
-    (uintptr_t)hardfault_handler, // 0x0000'00BC - TODO
+    // subsequent interrupts left unimplemented
+    // the bootloader won't be able to use these, but since the application maps its vector table
+    // to SRAM, it can change interrupt handlers from the defaults at runtime
+    // (see application's vector_table.c)
+    (uintptr_t)hardfault_handler,
+    (uintptr_t)hardfault_handler,
+    (uintptr_t)hardfault_handler,
+    (uintptr_t)hardfault_handler,
+    (uintptr_t)hardfault_handler,
+    (uintptr_t)hardfault_handler,
+    (uintptr_t)hardfault_handler,
+    (uintptr_t)hardfault_handler,
+    (uintptr_t)hardfault_handler,
+    (uintptr_t)hardfault_handler,
+    (uintptr_t)hardfault_handler,
+    (uintptr_t)hardfault_handler,
+    (uintptr_t)hardfault_handler,
+    (uintptr_t)hardfault_handler,
+    (uintptr_t)hardfault_handler,
+    (uintptr_t)hardfault_handler,
+    (uintptr_t)hardfault_handler,
+    (uintptr_t)hardfault_handler,
+    (uintptr_t)hardfault_handler,
+    (uintptr_t)hardfault_handler,
+    (uintptr_t)hardfault_handler,
+    (uintptr_t)hardfault_handler,
+    (uintptr_t)hardfault_handler,
+    (uintptr_t)hardfault_handler,
+    (uintptr_t)hardfault_handler,
+    (uintptr_t)hardfault_handler,
+    (uintptr_t)hardfault_handler,
+    (uintptr_t)hardfault_handler,
+    (uintptr_t)hardfault_handler,
+    (uintptr_t)hardfault_handler,
+    (uintptr_t)hardfault_handler,
+    (uintptr_t)hardfault_handler,
+    (uintptr_t)hardfault_handler,
+    (uintptr_t)hardfault_handler,
+    (uintptr_t)hardfault_handler,
+    (uintptr_t)hardfault_handler,
+    (uintptr_t)hardfault_handler,
+    (uintptr_t)hardfault_handler,
+    (uintptr_t)hardfault_handler,
+    (uintptr_t)hardfault_handler,
+    (uintptr_t)hardfault_handler,
 };
