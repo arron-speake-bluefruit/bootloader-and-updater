ed96e4a0062681903c0f6e415067af48e22bd4de
Arron Speake <arron.speake@bluefruit.co.uk>
2024-05-03 18:30:19 +0100

    Refactor flash_copy functions

--- libraries/flash_copy/flash_copy.c
+++ libraries/flash_copy/flash_copy.c
@@ -5,14 +5,12 @@ static void wait_until_flash_not_busy(void) {
     while (flash_is_busy()) {}
 }
 
-bool flash_erase_pages(uint32_t page_address, uint32_t page_count) {
-    flash_unlock(FLASH_KEY1, FLASH_KEY2);
-    wait_until_flash_not_busy();
+static bool erase_pages_unlocked(uint32_t page_address, uint32_t page_count) {
+    flash_set_operation(flash_operation_page_erase);
 
     for (size_t i = 0; i < page_count; i++) {
         uint32_t page = page_address + (i * flash_page_size);
 
-        flash_set_operation(flash_operation_page_erase);
         flash_set_address(page);
         flash_operation_start();
 
@@ -20,30 +18,52 @@ bool flash_erase_pages(uint32_t page_address, uint32_t page_count) {
         uint32_t status = flash_status();
         flash_clear_status();
 
-        bool programming_error = (status & flash_status_bit_programming_error) != 0;
-        bool write_protect_error = (status & flash_status_bit_write_protect_error) != 0;
-        if (programming_error || write_protect_error) {
-            flash_lock();
+        bool success = (status & flash_status_bit_end_of_program) != 0;
+        if (!success) {
             return false;
         }
     }
 
-    flash_lock();
     return true;
 }
 
-void flash_copy(const uint16_t* source, uint16_t* destination, size_t count) {
-    flash_unlock(FLASH_KEY1, FLASH_KEY2);
-    wait_until_flash_not_busy(); // TODO?
+static bool copy_unlocked(const uint16_t* source, uint16_t* destination, size_t count) {
+    flash_set_operation(flash_operation_program);
 
     for (size_t i = 0; i < count; i++) {
-        flash_set_operation(flash_operation_program);
         destination[i] = source[i];
 
         wait_until_flash_not_busy();
+        uint32_t status = flash_status();
+        flash_clear_status();
 
-        // TODO: Check for failure.
+        bool success = (status & flash_status_bit_end_of_program) != 0;
+        if (!success) {
+            return false;
+        }
     }
 
+    return true;
+}
+
+bool flash_erase_pages(uint32_t page_address, uint32_t page_count) {
+    flash_unlock(FLASH_KEY1, FLASH_KEY2);
+    wait_until_flash_not_busy();
+
+    bool success = erase_pages_unlocked(page_address, page_count);
+
     flash_lock();
+
+    return success;
+}
+
+bool flash_copy(const uint16_t* source, uint16_t* destination, size_t count) {
+    flash_unlock(FLASH_KEY1, FLASH_KEY2);
+    wait_until_flash_not_busy();
+
+    bool success = copy_unlocked(source, destination, count);
+
+    flash_lock();
+
+    return success;
 }
--- libraries/flash_copy/include/flash_copy.h
+++ libraries/flash_copy/include/flash_copy.h
@@ -11,10 +11,18 @@ enum {
 };
 
 // Erase the `page_count` pages in flash, starting at `page_address`.
+//
+// If the operation succeeds, all pages in the given range will contain half-words of `0xFFFF`,
+// returning `true`. If the operation fails, pages in the range will be in an undefined state,
+// returning `false`.
 bool flash_erase_pages(uint32_t page_address, uint32_t page_count);
 
 // Copy `count` half-words from `source` into `destination`. The destination range MUST fit into
 // an erased flash region.
-void flash_copy(const uint16_t* source, uint16_t* destination, size_t count);
+//
+// If the operation succeeds, all half-words in `[source, source+count)` will be equal to
+// `[destination, destination+count)`, returning `true`. If the operation fails, the destination
+// range will be in an undefined state, returnin `false`.
+bool flash_copy(const uint16_t* source, uint16_t* destination, size_t count);
 
 #endif // FLASH_COPY_H
